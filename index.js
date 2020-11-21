dccf22357ac826ba6d03eaf68d154e9d2e37aec4
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-11-21 08:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
   if (left > 0) {  dodger.style.left = `${left - 1}px`;
-}
+}}
 
 function moveDodgerRight(){
   let rightNumbers = dodger.style.left.replace("px","");
@@ -16,4 +16,4 @@ function moveDodgerRight(){
 document.addEventListener("keydown",function(e){
   if(e.key ==="ArrowRight") moveDodgerRight();
   else if (e.key === "ArrowLeft") moveDodgerLeft();
-})}
+});
